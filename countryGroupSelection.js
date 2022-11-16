f0b9cb5b9ec31e3c80e45cf42050893598e6cb10
philipgaje <106719876+philipgaje@users.noreply.github.com>
2022-11-16 09:43:44 +0800

    Update countryGroupSelection.js

--- countryGroupSelection.js
+++ countryGroupSelection.js
@@ -1,3 +1,32 @@
 function test() {
   alert("Testing external js file");
 }
+
+function getAccessToken() {
+  var body = {
+    "email": "chrisbrewer@worldfamilyenglish.com",
+    "password": "worldfamilychris!#"
+  };
+
+  $.ajax({
+      url: 'https://apps.dwe.hk/wfhk/api/auth/token/generate',
+      type: 'POST',
+      dataType: 'json',
+      contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
+      data: body,
+      complete: function(result) {
+          //called when complete
+          alert(result);
+      },
+
+      success: function(result) {
+          //called when successful
+          alert(result);
+      },
+
+      error: function(result) {
+          //called when there is an error
+          alert(result);
+      },
+  });
+}
