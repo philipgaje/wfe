e84bbde54b4deeb1d8afba89b7b910079e73214e
philipgaje <106719876+philipgaje@users.noreply.github.com>
2022-11-16 13:18:16 +0800

    Update countryGroupSelection.js

--- countryGroupSelection.js
+++ countryGroupSelection.js
@@ -1,3 +1,5 @@
+var token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczpcL1wvYXBwcy5kd2UuaGtcL3dmaGtcL2FwaVwvYXV0aFwvdG9rZW5cL2dlbmVyYXRlIiwiaWF0IjoxNjY4NTU4NjAxLCJleHAiOjE2NzA5Nzc4MDEsIm5iZiI6MTY2ODU1ODYwMSwianRpIjoiVXo5ZFJrOGpkSGlnTnRWTSIsInN1YiI6MywicHJ2IjoiMjNiZDVjODk0OWY2MDBhZGIzOWU3MDFjNDAwODcyZGI3YTU5NzZmNyJ9.omzXkCre6BMtftsnuXFgRVscvLnsx8VP9U3uc2Lrbpg';
+
 function setCountryGroupOptions() {
   let dropdown = $("#區域");
   
@@ -7,17 +9,27 @@ function setCountryGroupOptions() {
   
   dropdown.attr('selectedIndex', 0);
   
+  getAccessToken();
+  
+//   if(token) {
+//     getAccessToken().then(function () {
+//       // fetch country groups
+//     }
+//   }
+                          
+  // fetch country groups
+  
 //   dropdown.append($('<option></option>').attr('value', 1).text("Group 1"));
 //   dropdown.append($('<option></option>').attr('value', 2).text("Group 2"));
 //   dropdown.append($('<option></option>').attr('value', 3).text("Group 3"));
   
-  const url = "https://apps.dwe.hk/wfhk/api/leads/3/country_group"
+//   const url = "https://apps.dwe.hk/wfhk/api/leads/3/country_group"
   
-  $.getJSON(url, function (data) {
-    $.each(data, function (key, entry) {
-      dropdown.append($('<option></option>').attr('value', entry.country_group_id).text(entry.name));
-    })
-  });
+//   $.getJSON(url, function (data) {
+//     $.each(data, function (key, entry) {
+//       dropdown.append($('<option></option>').attr('value', entry.country_group_id).text(entry.name));
+//     })
+//   });
 }
 
 function handleCountryGroupSelect() {
@@ -25,24 +37,24 @@ function handleCountryGroupSelect() {
   alert(country_group_id);
 }
 
-async function getAccessToken() {
+function getAccessToken() {
   var body = {
     "email": "chrisbrewer@worldfamilyenglish.com",
     "password": "worldfamilychris!#"
   };
   
-  const response = await $.ajax({
-      url: 'https://apps.dwe.hk/wfhk/api/auth/token/generate',
-      type: 'POST',
-      dataType: 'json',
-      contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
-      data: body,
-      complete: function(result) {},
-      success: function(result) {
-        console.log(result);
-      },
-      error: function(result) {},
-  });
-  
-  return response;
+  return fetch('https://apps.dwe.hk/wfhk/api/auth/token/generate', {
+    method: 'POST',
+    body: body,
+    headers: {
+			'Content-Type': 'application/json'
+		}
+  }).then(function (res) {
+      return res.json;
+  }).then(function (data) {
+		console.log('token', data);
+		token = data.token;
+  }).catch(function (err) {
+		console.log('something went wrong', err);
+	});
 }
