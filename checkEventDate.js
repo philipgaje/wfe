4cc07eeab0f250cbce70addc3bfcc821699a027d
philipgaje <106719876+philipgaje@users.noreply.github.com>
2022-11-24 11:16:17 +0800

    Update checkEventDate.js

--- checkEventDate.js
+++ checkEventDate.js
@@ -3,7 +3,40 @@
 //   const urlParams = new URLSearchParams(queryString);
 //   const trackingCode = urlParams.get("tracking_code");
   
-  const trackingCode = '2210rj';
+  const trackingCode = 'nv1806';
   
-  alert(trackingCode);
+  getEventDate(trackingCode);
 })();
+
+let token = null;
+
+function getEventDate(trackingCode) {
+  return fetch('https://apps.dwe.hk/wfhk/api/leads/3/info/' + trackingCode, {
+    headers: {
+      'Authorization': 'bearer ' + token,
+    }
+  }).then(function (res) {
+    return res.json();
+  }).then(function (data) {
+    console.log(data);
+  }).catch(function (err) {
+    console.log(err);
+  });
+}
+
+function getAccessToken() {
+  const email = "chrisbrewer@worldfamilyenglish.com";
+  const password = "worldfamilychris!#";
+  
+  return fetch('https://apps.dwe.hk/wfhk/api/auth/token/generate', {
+    method: 'POST',
+    body: "email=" + email + "&password=" + password,
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    }
+  }).then(function (res) {
+      return res.json();
+  }).then(function (data) {
+      token = data.token;
+  }).catch(function (err) {});
+}
